12f983ee1b036bd3088878bdc181d593abd4a844
Mikkaiser <mikaelrsimoes19@gmail.com>
2023-04-07 20:42:33 -0300

    add: new functions to separate responsibilities

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+const emailElement = document.getElementById("email");
+const passwordElement = document.getElementById("password");
 const form = document.querySelector("form");
 const messageErrorEmail = document.getElementById("message-error-email");
 const messageErrorPassword = document.getElementById("message-error-password");
@@ -7,12 +7,16 @@ const messageErrorPassword = document.getElementById("message-error-password");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  verifyEmail();
+  verifyPassword();
+
+  form.reset();
+});
+
+const verifyEmail = () => {
   const regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-  const regexPassword =
-    /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{8,16}$/;
-
-  if (!regexEmail.test(email.value) || email.value == null || email.value == "") {
+  if (!regexEmail.test(emailElement.value) || emailElement.value == null || emailElement.value == "") {
     form.classList.add("message-invalid");
     messageErrorEmail.innerHTML =
       "Por favor, colocar um endereço de e-mail válido";
@@ -20,14 +24,19 @@ form.addEventListener("submit", (event) => {
     form.classList.remove("message-invalid");
     messageErrorEmail.innerHTML = "";
   }
+}
 
-  if (!regexPassword.test(password.value)) {
+const verifyPassword = () => {
+  const regexPassword =
+    /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{8,16}$/;
+
+  if (!regexPassword.test(passwordElement.value)) {
     form.classList.add("message-invalid");
     messageErrorPassword.innerHTML = "Senha inválida";
   } else {
     form.classList.remove("message-invalid");
     messageErrorPassword.innerHTML = "";
   }
+}
 
-  form.reset();
-});
+
