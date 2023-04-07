bd029b8c28dae0bbe4788a1c019da962594d95c9
Leticia Dias <contatoleticiadia@gmail.com>
2023-04-07 13:40:01 -0300

    feat(form): added new validation in form

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const form = document.getElementById('form');
-const messageError = document.getElementById('error');
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.querySelector("form");
+const messageErrorEmail = document.getElementById("message-error-email");
+const messageErrorPassword = document.getElementById("message-error-password");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-form.addEventListener('submit', (event) => {
-    let messages = [];
-    let regex = /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/;
-    
-    if(email.value === '' || email.value === null) {
-        messages.push('O campo e-mail é obrigatório');
-    }
+  const regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if(password.value.length <= 8) {
-        messages.push('A senha tem que ter mais de 8 caracteres');
-    }
+  const regexPassword =
+    /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{8,16}$/;
 
-    if(!regex.test(password.value)) {
-        messages.push('Precisa ter uma letra maiúscula e minúscula, caractere especial e número');
-    }
+  if (!regexEmail.test(email.value)) {
+    form.classList.add("message-invalid");
+    messageErrorEmail.innerHTML =
+      "Por favor, colocar um endereço de e-mail válido";
+  } else {
+    form.classList.remove("message-invalid");
+    messageErrorEmail.innerHTML = "";
+  }
 
-    if(messages.length > 0) {
-        event.preventDefault();
-        messageError.innerHTML = messages.join(', ');
-    }
+  if (!regexPassword.test(password.value)) {
+    form.classList.add("message-invalid");
+    messageErrorPassword.innerHTML = "Senha inválida";
+  } else {
+    form.classList.remove("message-invalid");
+    messageErrorPassword.innerHTML = "";
+  }
+
+  form.reset();
 });
